6ec32d5795a3ab4460f861742841af70ffeb16b6
faishalimam38 <faishalimam38@gmail.com>
2021-04-23 23:38:57 +0530

    changing sdata

---
+++ src/Sdata.jsx
@@ -0,0 +1,31 @@
+const Sdata =[
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title:  " A Netflix Original series",
+        sname:  "DARK",
+        link:  "https://www.netflix.com/in/title/80990668?source=35"
+    },
+
+    {
+        imgsrc: "https://wallpaperaccess.com/full/4600591.jpg",
+      title: " A Netflix Original series",
+      sname: "Extracurricular",
+      link:  "https://www.netflix.com/in/title/80990668?source=35"
+    },
+
+    {
+        imgsrc:   "https://content.vidangel.com/uploads/2019/07/x2LSRK2Cm7MZhjluni1msVJ3wDF.poster.jpg",
+        title:   " A Netflix Original series",
+        sname:   "Stranger at night",
+        link:   "https://www.netflix.com/in/title/80990668?source=35"
+    },
+
+    {
+        imgsrc:   "https://content.vidangel.com/uploads/2019/07/x2LSRK2Cm7MZhjluni1msVJ3wDF.poster.jpg",
+        title:   " A Netflix Original series",
+        sname:   "Stranger at night",
+        link:   "https://www.netflix.com/in/title/80990668?source=35"
+    },
+]
+
+export default Sdata
--- src/index.js
+++ src/index.js
@@ -2,34 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from "./Cards";
+import Sdata from "./Sdata"
+
+
+// function ncard(val, index, arr) {
+
+// }
 
 ReactDOM.render(
   <>
 
     <h1 className="heading_style"> List of top 3 Netflix Series in 2021 </h1>
 
+    {Sdata.map((val) => {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
 
 
-    <Card
-      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-      title=" A Netflix Original series"
-      sname="DARK"
-      link="https://www.netflix.com/in/title/80990668?source=35"
-    />
 
-    <Card
-      imgsrc="https://wallpaperaccess.com/full/4600591.jpg"
-      title=" A Netflix Original series"
-      sname="Extracurricular"
-      link="https://www.netflix.com/in/title/80990668?source=35"
-    />
 
-    <Card
-      imgsrc="https://content.vidangel.com/uploads/2019/07/x2LSRK2Cm7MZhjluni1msVJ3wDF.poster.jpg"
-      title=" A Netflix Original series"
-      sname="Stranger at night"
-      link="https://www.netflix.com/in/title/80990668?source=35"
-    />
   </>,
 
 
